bfa3794a9c9f216dc19934fe339b0b89a5ba2d9e
Kamil Wojtczak <kamil.wojtczak1@gmail.com>
2018-02-24 18:47:44 +0100

    Add files

---
+++ Board.js
@@ -0,0 +1,42 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': 'X-Client-Id',
+    'X-Auth-Token': 'X-Auth-Token'
+};
+
+var board = {
+	name: 'Tablica Kanban',
+	createColumn: function(column) {
+	  this.element.append(column.element);
+	  initSortable();
+	},
+	element: $('#board .column-container')
+};
+$('.create-column').click(function() {
+    var columnName = prompt('Enter a new column name');
+    board.createColumn(new Column(columnName));
+});
+
+
+$('.create-column')
+	.click(function(){
+		var columnName = prompt('Enter a column name');
+        $.ajax({
+            url: baseUrl + '/column',
+            method: 'POST',
+            data: {
+                name: columnName
+            },
+            success: function(response) {
+                var Column = new Column (response.id, columnName);
+                board.createColumn(column);
+            }
+        });
+	});
+	
+function initSortable() {
+    $('.card-list').sortable({
+      connectWith: '.card-list',
+      placeholder: 'card-placeholder'
+    }).disableSelection();
+  }
